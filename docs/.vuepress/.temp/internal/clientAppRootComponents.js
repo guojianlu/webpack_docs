c8a6d15a0aebc1353dd535b54cd4dd298ade137e
tangli <candy@192.168.0.101>
2022-08-21 20:14:37 +0800

    initial repo

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from '/Users/candy/Desktop/webpack5/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
